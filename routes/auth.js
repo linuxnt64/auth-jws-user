96779190a3174c5a13e4589e6c35793bb4650c33
Staffan Simmons <staffan.simmons@gmail.com>
2021-11-01 16:03:18 +0100

    debug: console loggar loginObject

--- routes/auth.js
+++ routes/auth.js
@@ -44,6 +44,7 @@ router.post('/register', async (req, res) => {
 
 
 router.post('/login', async (req, res) => {
+    console.log(req.body);
     //validate user
     const { error } = loginValidation(req.body);
     if (error) {
