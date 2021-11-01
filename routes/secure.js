3ed5d2f5deea15d95617394805f7f1efcdb25e48
Staffan Simmons <staffan.simmons@gmail.com>
2021-11-01 14:41:53 +0100

    .

--- routes/secure.js
+++ routes/secure.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const verify = require('./verifyToken');
+const verify = require('./verifytoken');
 
 router.get('/', verify, (req,res)=> { 
     res.redirect('/batcave');
