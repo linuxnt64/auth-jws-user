7c690b87e19390285a80cc7b30e32ea0d1854920
Staffan Simmons <staffan.simmons@gmail.com>
2021-11-03 10:24:09 +0100

    försöker få igång heroku

--- routes/auth.js
+++ routes/auth.js
@@ -49,15 +49,14 @@ router.post('/login', async (req, res) => {
     const { error } = loginValidation(req.body);
     if (error) {
         res.status(400).json({ error: error.details[0].message });
-        console.log(error.details[0].message);
     }
-
+/* 
     const user = await User.findOne({ email: req.body.email });
     if (!user) {
         return res.status(400).json({ error: 'Email not found' });
     }
-
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
+ */
+    const validPassword = await bcrypt.compare(req.body.password, User.password);
     if (!validPassword) {
         return res.status(400).json({ error: 'Invalid password' });
     }
